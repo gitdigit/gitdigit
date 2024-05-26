3083ce324df1281742bc088d1c26ae7904cb41f9
Jennifer <96658538+gitdigit@users.noreply.github.com>
2024-05-27 00:18:43 +0300

    Create readme.js

---
+++ readme.js
@@ -0,0 +1,26 @@
+module.exports = `### Hey, I'm Jennifer 👋🏽  
+
+I'm a 21 years old engineering student based in France, and I'm passionate about crafting digital experiences and solutions.
+ 
+## 📦 Toolbox
+
+**Game development and data analysis:** `C` `C++` `Python` `Arduino` 
+
+**Frontend Development:**  `JavaScript` `HTML` `CSS` 
+ 
+**Version Control:** `Git` `Github`
+
+**Backend Development:** `SQL`  `PHP`   `MySQL` `JavaScript`
+
+**Others:**  `Notion`  `Agile`  `Scrum` `Jira`
+ 
+## ✨ Fun Facts 
+
+- I love running, and watching movies.
+- As a kid, I wanted to be an air hostess.
+- I love travelling.
+
+## 📫 Contact
+
+ For a quick response, DM me on [LinkedIn](https://www.linkedin.com/in/jennifer-kouassi-534434233/). 
+ `;
